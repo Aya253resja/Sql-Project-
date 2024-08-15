7af1385a6daa23c34a3c5b271d7a384926da958d
Aya Gomaa <115928828+Aya253resja@users.noreply.github.com>
2024-08-16 02:46:36 +0300

    Add files via upload

---
+++ Dept Insertion.sql
@@ -0,0 +1,13 @@
+INSERT INTO [Department] (Dept_ID, Dept_Name, Branch_ID)
+VALUES
+(7004, 'Computer Science', 5007),
+(7005, 'IOT',5008 ),
+(7006, 'Cybersecurity',5009 ),
+(7007, 'DESIGN',5010 ),
+(7008, 'Data Science', 5011),
+(7009, 'Networking', 5012),
+(7010, 'Artificial Intelligence and Machine Learning', 5013),
+(7011, 'Robotics', 5004),
+(7012, 'Electrical and Computer Engineering', 5006),
+(7013, 'Cloud Computing and Big Data', 5005);
+-------------------------------------------
